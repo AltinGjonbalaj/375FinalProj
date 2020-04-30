eb13547287acab49dbe63c476f798f0f890224b4
Stephanie Greenberg <sgreenb8@binghamton.edu>
2020-04-30 17:15:53 -0400

    less nodes/trees

--- sparse_input_gen.cpp
+++ sparse_input_gen.cpp
@@ -9,8 +9,8 @@ using namespace std;
 int main() {
 	ofstream output("sparse_input.txt");
 	srand((unsigned)time(0));
-	for (int i = 0; i < 1000000; i++) {
-		const int num_nodes = (rand() % ((100 - 3) + 1)) + 3; //pick a number of nodes between 3 and 100
+	for (int i = 0; i < 500000; i++) {
+		const int num_nodes = (rand() % ((50 - 3) + 1)) + 3; //pick a number of nodes between 3 and 100
 		int adj_matrix[num_nodes][num_nodes];
 		for (int row = 0; row < num_nodes; row++) {
 			for (int col = 0; col <= row; col++) {
